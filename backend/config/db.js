bf847da078457230baf88865ecd49dc133f5fd92
Master-Muhammad-Maaz <mohammadmaaz8262@gmail.com>
2026-03-24 16:17:33 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -42,7 +42,7 @@ import { ReadPreference, type ReadPreferenceLike } from './read_preference';
 import { DEFAULT_PK_FACTORY, filterOptions, MongoDBNamespace, resolveOptions } from './utils';
 import { WriteConcern, type WriteConcernOptions } from './write_concern';
 
-// Allowed parameters
+// Allowed parameters for database configuration
 const DB_OPTIONS_ALLOW_LIST = [
   'writeConcern',
   'readPreference',
@@ -81,55 +81,24 @@ export interface DbPrivate {
 
 /** @public */
 export interface DbOptions extends BSONSerializeOptions, WriteConcernOptions {
-  /** If the database authentication is dependent on another databaseName. */
   authSource?: string;
-  /** Force server to assign _id values instead of driver. */
   forceServerObjectId?: boolean;
-  /** The preferred read preference (ReadPreference.PRIMARY, ReadPreference.PRIMARY_PREFERRED, ReadPreference.SECONDARY, ReadPreference.SECONDARY_PREFERRED, ReadPreference.NEAREST). */
   readPreference?: ReadPreferenceLike;
-  /** A primary key factory object for generation of custom _id keys. */
   pkFactory?: PkFactory;
-  /** Specify a read concern for the collection. (only MongoDB 3.2 or higher supported) */
   readConcern?: ReadConcern;
-  /** Should retry failed writes */
   retryWrites?: boolean;
-  /**
-   * @experimental
-   * Specifies the time an operation will run until it throws a timeout error
-   */
+  /** Specifies the time an operation will run until it throws a timeout error */
   timeoutMS?: number;
 }
 
 /**
- * The **Db** class is a class that represents a MongoDB Database.
- * @public
- *
- * @example
- * ```ts
- * import { MongoClient } from 'mongodb';
- *
- * interface Pet {
- *   name: string;
- *   kind: 'dog' | 'cat' | 'fish';
- * }
- *
- * const client = new MongoClient('mongodb://localhost:27017');
- * const db = client.db();
- *
- * // Create a collection that validates our union
- * await db.createCollection<Pet>('pets', {
- *   validator: { $expr: { $in: ['$kind', ['dog', 'cat', 'fish']] } }
- * })
- * ```
+ * The **Db** class represents a MongoDB Database.
  */
 export class Db {
   /** @internal */
   s: DbPrivate;
 
-  /**
-   * Gets the MongoClient associated with the Db.
-   * @public
-   */
+  /** Gets the MongoClient associated with the Db. */
   readonly client: MongoClient;
 
   public static SYSTEM_NAMESPACE_COLLECTION = CONSTANTS.SYSTEM_NAMESPACE_COLLECTION;
@@ -141,38 +110,22 @@ export class Db {
 
   /**
    * Creates a new Db instance.
-   *
-   * Db name cannot contain a dot, the server may apply more restrictions when an operation is run.
-   *
-   * @param client - The MongoClient for the database.
-   * @param databaseName - The name of the database this instance represents.
-   * @param options - Optional settings for Db construction.
    */
   constructor(client: MongoClient, databaseName: string, options?: DbOptions) {
     options = options ?? {};
-
-    // Filter the options
     options = filterOptions(options, DB_OPTIONS_ALLOW_LIST);
 
-    // Ensure there are no dots in database name
     if (typeof databaseName === 'string' && databaseName.includes('.')) {
       throw new MongoInvalidArgumentError(`Database names cannot contain the character '.'`);
     }
 
-    // Internal state of the db object
     this.s = {
-      // Options
       options,
-      // Unpack read preference
       readPreference: ReadPreference.fromOptions(options),
-      // Merge bson options
       bsonOptions: resolveBSONOptions(options, client),
-      // Set up the primary key factory or fallback to ObjectId
       pkFactory: options?.pkFactory ?? DEFAULT_PK_FACTORY,
-      // ReadConcern
       readConcern: ReadConcern.fromOptions(options),
       writeConcern: WriteConcern.fromOptions(options),
-      // Namespace
       namespace: new MongoDBNamespace(databaseName)
     };
 
@@ -183,14 +136,10 @@ export class Db {
     return this.s.namespace.db;
   }
 
-  // Options
   get options(): DbOptions | undefined {
     return this.s.options;
   }
 
-  /**
-   * Check if a secondary can be used (because the read preference is *not* set to primary)
-   */
   get secondaryOk(): boolean {
     return this.s.readPreference?.preference !== 'primary' || false;
   }
@@ -199,23 +148,14 @@ export class Db {
     return this.s.readConcern;
   }
 
-  /**
-   * The current readPreference of the Db. If not explicitly defined for
-   * this Db, will be inherited from the parent MongoClient
-   */
   get readPreference(): ReadPreference {
-    if (this.s.readPreference == null) {
-      return this.client.readPreference;
-    }
-
-    return this.s.readPreference;
+    return this.s.readPreference ?? this.client.readPreference;
   }
 
   get bsonOptions(): BSONSerializeOptions {
     return this.s.bsonOptions;
   }
 
-  // get the write Concern
   get writeConcern(): WriteConcern | undefined {
     return this.s.writeConcern;
   }
@@ -229,13 +169,7 @@ export class Db {
   }
 
   /**
-   * Create a new collection on a server with the specified options. Use this to create capped collections.
-   * More information about command options available at https://www.mongodb.com/docs/manual/reference/command/create/
-   *
-   * Collection namespace validation is performed server-side.
-   *
-   * @param name - The name of the collection to create
-   * @param options - Optional settings for the command
+   * Create a new collection on a server.
    */
   async createCollection<TSchema extends Document = Document>(
     name: string,
@@ -246,32 +180,9 @@ export class Db {
   }
 
   /**
-   * Execute a command
-   *
-   * @remarks
-   * This command does not inherit options from the MongoClient.
-   *
-   * The driver will ensure the following fields are attached to the command sent to the server:
-   * - `lsid` - sourced from an implicit session or options.session
-   * - `$readPreference` - defaults to primary or can be configured by options.readPreference
-   * - `$db` - sourced from the name of this database
-   *
-   * If the client has a serverApi setting:
-   * - `apiVersion`
-   * - `apiStrict`
-   * - `apiDeprecationErrors`
-   *
-   * When in a transaction:
-   * - `readConcern` - sourced from readConcern set on the TransactionOptions
-   * - `writeConcern` - sourced from writeConcern set on the TransactionOptions
-   *
-   * Attaching any of the above fields to the command will have no effect as the driver will overwrite the value.
-   *
-   * @param command - The command to run
-   * @param options - Optional settings for the command
+   * Execute a command against the database.
    */
   async command(command: Document, options?: RunCommandOptions & Abortable): Promise<Document> {
-    // Intentionally, we do not inherit options from parent for this operation.
     return await executeOperation(
       this.client,
       new RunCommandOperation(
@@ -289,10 +200,7 @@ export class Db {
   }
 
   /**
-   * Execute an aggregation framework pipeline against the database.
-   *
-   * @param pipeline - An array of aggregation stages to be executed
-   * @param options - Optional settings for the command
+   * Execute an aggregation framework pipeline.
    */
   aggregate<T extends Document = Document>(
     pipeline: Document[] = [],
@@ -312,12 +220,7 @@ export class Db {
   }
 
   /**
-   * Returns a reference to a MongoDB Collection. If it does not exist it will be created implicitly.
-   *
-   * Collection namespace validation is performed server-side.
-   *
-   * @param name - the collection name we wish to access.
-   * @returns return the new Collection instance
+   * Returns a reference to a MongoDB Collection.
    */
   collection<TSchema extends Document = Document>(
     name: string,
@@ -329,11 +232,7 @@ export class Db {
     return new Collection<TSchema>(this, name, resolveOptions(this, options));
   }
 
-  /**
-   * Get all the db statistics.
-   *
-   * @param options - Optional settings for the command
-   */
+  /** Get all database statistics. */
   async stats(options?: DbStatsOptions): Promise<Document> {
     return await executeOperation(
       this.client,
@@ -341,25 +240,7 @@ export class Db {
     );
   }
 
-  /**
-   * List all collections of this database with optional filter
-   *
-   * @param filter - Query to filter collections by
-   * @param options - Optional settings for the command
-   */
-  listCollections(
-    filter: Document,
-    options: Exclude<ListCollectionsOptions, 'nameOnly'> & { nameOnly: true } & Abortable
-  ): ListCollectionsCursor<Pick<CollectionInfo, 'name' | 'type'>>;
-  listCollections(
-    filter: Document,
-    options: Exclude<ListCollectionsOptions, 'nameOnly'> & { nameOnly: false } & Abortable
-  ): ListCollectionsCursor<CollectionInfo>;
-  listCollections<
-    T extends Pick<CollectionInfo, 'name' | 'type'> | CollectionInfo =
-      | Pick<CollectionInfo, 'name' | 'type'>
-      | CollectionInfo
-  >(filter?: Document, options?: ListCollectionsOptions & Abortable): ListCollectionsCursor<T>;
+  /** List collections with optional filters. */
   listCollections<
     T extends Pick<CollectionInfo, 'name' | 'type'> | CollectionInfo =
       | Pick<CollectionInfo, 'name' | 'type'>
@@ -371,22 +252,12 @@ export class Db {
     return new ListCollectionsCursor<T>(this, filter, resolveOptions(this, options));
   }
 
-  /**
-   * Rename a collection.
-   *
-   * @remarks
-   * This operation does not inherit options from the MongoClient.
-   *
-   * @param fromCollection - Name of current collection to rename
-   * @param toCollection - New name of of the collection
-   * @param options - Optional settings for the command
-   */
+  /** Rename a collection. */
   async renameCollection<TSchema extends Document = Document>(
     fromCollection: string,
     toCollection: string,
     options?: RenameOptions
   ): Promise<Collection<TSchema>> {
-    // Intentionally, we do not inherit options from parent for this operation.
     return await executeOperation(
       this.client,
       new RenameOperation(
@@ -400,22 +271,13 @@ export class Db {
     );
   }
 
-  /**
-   * Drop a collection from the database, removing it permanently. New accesses will create a new collection.
-   *
-   * @param name - Name of collection to drop
-   * @param options - Optional settings for the command
-   */
+  /** Drop a collection from the database. */
   async dropCollection(name: string, options?: DropCollectionOptions): Promise<boolean> {
     options = resolveOptions(this, options);
     return await dropCollections(this, name, options);
   }
 
-  /**
-   * Drop a database, removing it permanently from the server.
-   *
-   * @param options - Optional settings for the command
-   */
+  /** Drop the entire database. */
   async dropDatabase(options?: DropDatabaseOptions): Promise<boolean> {
     return await executeOperation(
       this.client,
@@ -423,30 +285,17 @@ export class Db {
     );
   }
 
-  /**
-   * Fetch all collections for the current db.
-   *
-   * @param options - Optional settings for the command
-   */
+  /** Fetch all valid collections for the current db. */
   async collections(options?: ListCollectionsOptions): Promise<Collection[]> {
     options = resolveOptions(this, options);
     const collections = await this.listCollections({}, { ...options, nameOnly: true }).toArray();
 
     return collections
-      .filter(
-        // Filter collections removing any illegal ones
-        ({ name }) => !name.includes('$')
-      )
+      .filter(({ name }) => !name.includes('$'))
       .map(({ name }) => new Collection(this, name, this.s.options));
   }
 
-  /**
-   * Creates an index on the db and collection.
-   *
-   * @param name - Name of the collection to create the index on.
-   * @param indexSpec - Specify the field to index, or an index specification
-   * @param options - Optional settings for the command
-   */
+  /** Create an index on the specified collection. */
   async createIndex(
     name: string,
     indexSpec: IndexSpecification,
@@ -459,12 +308,7 @@ export class Db {
     return indexes[0];
   }
 
-  /**
-   * Remove a user from a database
-   *
-   * @param username - The username to remove
-   * @param options - Optional settings for the command
-   */
+  /** Remove a user from the database. */
   async removeUser(username: string, options?: RemoveUserOptions): Promise<boolean> {
     return await executeOperation(
       this.client,
@@ -472,12 +316,7 @@ export class Db {
     );
   }
 
-  /**
-   * Set the current profiling level of MongoDB
-   *
-   * @param level - The new profiling level (off, slow_only, all).
-   * @param options - Optional settings for the command
-   */
+  /** Set the MongoDB profiling level. */
   async setProfilingLevel(
     level: ProfilingLevel,
     options?: SetProfilingLevelOptions
@@ -488,11 +327,7 @@ export class Db {
     );
   }
 
-  /**
-   * Retrieve the current profiling Level for MongoDB
-   *
-   * @param options - Optional settings for the command
-   */
+  /** Retrieve the current MongoDB profiling level. */
   async profilingLevel(options?: ProfilingLevelOptions): Promise<string> {
     return await executeOperation(
       this.client,
@@ -500,25 +335,7 @@ export class Db {
     );
   }
 
-  /**
-   * Retrieves this collections index info.
-   *
-   * @param name - The name of the collection.
-   * @param options - Optional settings for the command
-   */
-  indexInformation(
-    name: string,
-    options: IndexInformationOptions & { full: true }
-  ): Promise<IndexDescriptionInfo[]>;
-  indexInformation(
-    name: string,
-    options: IndexInformationOptions & { full?: false }
-  ): Promise<IndexDescriptionCompact>;
-  indexInformation(
-    name: string,
-    options: IndexInformationOptions
-  ): Promise<IndexDescriptionCompact | IndexDescriptionInfo[]>;
-  indexInformation(name: string): Promise<IndexDescriptionCompact>;
+  /** Retrieve index information for a collection. */
   async indexInformation(
     name: string,
     options?: IndexInformationOptions
@@ -526,95 +343,19 @@ export class Db {
     return await this.collection(name).indexInformation(resolveOptions(this, options));
   }
 
-  /**
-   * Create a new Change Stream, watching for new changes (insertions, updates,
-   * replacements, deletions, and invalidations) in this database. Will ignore all
-   * changes to system collections.
-   *
-   * @remarks
-   * watch() accepts two generic arguments for distinct use cases:
-   * - The first is to provide the schema that may be defined for all the collections within this database
-   * - The second is to override the shape of the change stream document entirely, if it is not provided the type will default to ChangeStreamDocument of the first argument
-   *
-   * @remarks
-   * When `timeoutMS` is configured for a change stream, it will have different behaviour depending
-   * on whether the change stream is in iterator mode or emitter mode. In both cases, a change
-   * stream will time out if it does not receive a change event within `timeoutMS` of the last change
-   * event.
-   *
-   * Note that if a change stream is consistently timing out when watching a collection, database or
-   * client that is being changed, then this may be due to the server timing out before it can finish
-   * processing the existing oplog. To address this, restart the change stream with a higher
-   * `timeoutMS`.
-   *
-   * If the change stream times out the initial aggregate operation to establish the change stream on
-   * the server, then the client will close the change stream. If the getMore calls to the server
-   * time out, then the change stream will be left open, but will throw a MongoOperationTimeoutError
-   * when in iterator mode and emit an error event that returns a MongoOperationTimeoutError in
-   * emitter mode.
-   *
-   * To determine whether or not the change stream is still open following a timeout, check the
-   * {@link ChangeStream.closed} getter.
-   *
-   * @example
-   * In iterator mode, if a next() call throws a timeout error, it will attempt to resume the change stream.
-   * The next call can just be retried after this succeeds.
-   * ```ts
-   * const changeStream = collection.watch([], { timeoutMS: 100 });
-   * try {
-   *     await changeStream.next();
-   * } catch (e) {
-   *     if (e instanceof MongoOperationTimeoutError && !changeStream.closed) {
-   *       await changeStream.next();
-   *     }
-   *     throw e;
-   * }
-   * ```
-   *
-   * @example
-   * In emitter mode, if the change stream goes `timeoutMS` without emitting a change event, it will
-   * emit an error event that returns a MongoOperationTimeoutError, but will not close the change
-   * stream unless the resume attempt fails. There is no need to re-establish change listeners as
-   * this will automatically continue emitting change events once the resume attempt completes.
-   *
-   * ```ts
-   * const changeStream = collection.watch([], { timeoutMS: 100 });
-   * changeStream.on('change', console.log);
-   * changeStream.on('error', e => {
-   *     if (e instanceof MongoOperationTimeoutError && !changeStream.closed) {
-   *         // do nothing
-   *     } else {
-   *         changeStream.close();
-   *     }
-   * });
-   * ```
-   * @param pipeline - An array of {@link https://www.mongodb.com/docs/manual/reference/operator/aggregation-pipeline/|aggregation pipeline stages} through which to pass change stream documents. This allows for filtering (using $match) and manipulating the change stream documents.
-   * @param options - Optional settings for the command
-   * @typeParam TSchema - Type of the data being detected by the change stream
-   * @typeParam TChange - Type of the whole change stream document emitted
-   */
+  /** Create a new Change Stream for the database. */
   watch<
     TSchema extends Document = Document,
     TChange extends Document = ChangeStreamDocument<TSchema>
   >(pipeline: Document[] = [], options: ChangeStreamOptions = {}): ChangeStream<TSchema, TChange> {
-    // Allow optionally not specifying a pipeline
     if (!Array.isArray(pipeline)) {
       options = pipeline;
       pipeline = [];
     }
-
     return new ChangeStream<TSchema, TChange>(this, pipeline, resolveOptions(this, options));
   }
 
-  /**
-   * A low level cursor API providing basic driver functionality:
-   * - ClientSession management
-   * - ReadPreference for server selection
-   * - Running getMores automatically when a local batch is exhausted
-   *
-   * @param command - The command that will start a cursor on the server.
-   * @param options - Configurations for running the command, bson options will apply to getMores
-   */
+  /** A low-level cursor API for running commands. */
   runCursorCommand(command: Document, options?: RunCursorCommandOptions): RunCommandCursor {
     return new RunCommandCursor(this, command, options);
   }
