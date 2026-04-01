b7f858685749039310cdbbcd656102cf287d450d
Master-Muhammad-Maaz <mohammadmaaz8262@gmail.com>
2026-04-01 19:32:56 +0530

    Update StudentRecord.js

--- backend/models/StudentRecord.js
+++ backend/models/StudentRecord.js
@@ -1,32 +1,17 @@
 const mongoose = require("mongoose");
 
 const studentRecordSchema = new mongoose.Schema({
-  srNo: { 
-    type: Number, 
-    required: true 
-  },
-  name: { 
-    type: String, 
-    required: true, 
-    trim: true 
-  },
-  dob: { 
-    type: String, 
-    required: true 
-  },
-  mobile: { 
-    type: String, 
-    required: true 
-  },
+  srNo: { type: Number, required: true },
+  name: { type: String, required: true, trim: true },
+  dob: { type: String, required: true },
+  mobile: { type: String, required: true },
   course: { 
     type: String, 
     required: true, 
-    enum: ["B.Sc-I", "B.Sc-II", "B.Sc-III", "M.Sc-I", "M.Sc-II"],
-    default: "B.Sc-I"
+    enum: ["B.Sc-I", "B.Sc-II", "B.Sc-III", "M.Sc-I", "M.Sc-II"]
   }
 }, { timestamps: true });
 
-// Ensure unique combination of SR No and Course
 studentRecordSchema.index({ srNo: 1, course: 1 }, { unique: true });
 
 module.exports = mongoose.model("StudentRecord", studentRecordSchema);
